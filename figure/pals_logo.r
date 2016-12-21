78f2ce98a2deae897b4842e0b5dad0ae631228f4
Kevin Wright <kw.stat@gmail.com>
2016-12-21 10:02:33 -0600

    Update logo

--- figure/pals_logo.r
+++ figure/pals_logo.r
@@ -1,7 +1,5 @@
 # pals_logo.r
-# Time-stamp: <20 Dec 2016 15:08:38 c:/x/rpack/pals/figure/pals_logo.r>
-
-lib(pals)
+# Time-stamp: <20 Dec 2016 22:48:52 c:/x/rpack/pals/figure/pals_logo.r>
 
 # outline text demo
 lib(TeachingDemos)
@@ -12,25 +10,36 @@ shadowtext(5,4, 'Test 2', col='lightsteelblue')
 
 # hexagon demo
 lib(plotrix)
-plot(1:10)
+
+par(pty="s")
+plot(1:10, xlim=c(1,10), ylim=c(1,10), type='l')
+points(10:1, type='l') # clearly not orthogonal
 hexagon(2,2,unit=7)
 
+# Another hexagon, better looking with equal-length sides
+library(spatstat)
+H <- hextess(square(1), 0.5)
+H$tiles <- H$tiles[[1]]
+plot(H[2]) # just the middle hexagon
+box()
 # ----------------------------------------------------------------------------
 
+# Version 1, pointy bottom & non-equal sides
+
 # Code for logo begins here
+lib(pals)
+lib(TeachingDemos)
+lib(plotrix)
+
+# Use this to make a small logo for github
 windows(width=2, height=2)
+
 op <- par(mar=c(0,0,0,0))
 plot(5,type="n",xlim=c(0,10), ylim=c(0,10), axes=FALSE, xlab="", ylab="",
      xaxs = "i" # no extra space
      )
- # run across the three primaries
-gradient.rect(0,0,10,10,
-              col=parula(150),
-              #reds=c(1,0),
-              #greens=c(seq(0,1,length=10),seq(1,0,length=10)),
-              #blues=c(0,1),
-              gradient="y")
-shadowtext(5, 4, 'pals', col='white', r=0.15,
+gradient.rect(0,0,10,10, col=parula(150), gradient="y")
+shadowtext(5, 4, 'pals', col='white', bg="gray30", r=0.1,
            theta= seq(pi/4, 2*pi, length.out=16), cex=4)
 hexagon(1,1,unit=8)
 # locator()
@@ -47,3 +56,30 @@ polygon(x=c(9, 10, 10, 9), y=c(0,0, 10,10), col="white",border="white")
 
 # Save as png
 # Edit in Gimp. Crop, shrink to 150 pixels wide
+
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+# Version 2, flat bottom, equal-length sides
+
+lib(pals)
+lib(TeachingDemos)
+lib(plotrix)
+library(spatstat)
+H <- hextess(square(1), 0.5)
+
+op <- par(mar=c(0,0,0,0))
+plot(H[2], xaxs="i", main="") # just the middle hexagon
+#box()
+gradient.rect(0,0.066987299,1,0.933012702,
+              col=parula(150), gradient="y", border=NA)
+#plot(H[2], xaxs="i", add=TRUE) # just the middle hexagon
+bot=0.066987299; top=0.933012701
+polygon(x=c(0, .25, 0), y=c(bot, bot, .5), col="white",border="white")
+polygon(x=c(.75, 1, 1), y=c(bot, bot, .5), col="white",border="white")
+polygon(x=c(1, 1, .75), y=c(.5, top, top), col="white",border="white")
+polygon(x=c(0, .25, 0), y=c(.5, top, top), col="white",border="white")
+shadowtext(.5, .4, 'pals', col='white', bg="gray30", r=0.15,
+           theta= seq(pi/4, 2*pi, length.out=4), cex=10)
+
+# Click Resize, Fit to window. Shrink window, save to png
